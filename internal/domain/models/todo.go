cf281a09aa477dcab18256942d150f9eb0666d4c
DENFNC <andreev.gamepro@gmail.com>
2025-04-18 17:12:35 +0300

    feat: Preparing the TodoHandler handler

--- internal/domain/models/todo.go
+++ internal/domain/models/todo.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Task struct {
+	ID          uint32
+	Title       string
+	Description string
+	Status      string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
--- internal/handlers/todo.go
+++ internal/handlers/todo.go
@@ -1,1 +1,49 @@
 package handlers
+
+import (
+	"github.com/DENFNC/ToDoList/internal/domain/models"
+	"github.com/gin-gonic/gin"
+)
+
+type TodoService interface {
+	CreateTask(
+		title string,
+		description string,
+	) (uint32, error)
+	GetTask(
+		taskID uint32,
+	) (models.Task, error)
+	UpdateTask(
+		title string,
+		description string,
+		status string,
+	) (uint32, error)
+	DeleteTask(
+		taskID uint32,
+	) (uint32, error)
+}
+
+type TodoHandler struct {
+	svc TodoService
+}
+
+func NewTodoHandler(service TodoService) *TodoHandler {
+	return &TodoHandler{
+		svc: service,
+	}
+}
+
+func (h *TodoHandler) Register(rg *gin.RouterGroup) {
+	todoRoute := rg.Group("/api/v1/tasks")
+	{
+		todoRoute.POST("/:")
+		todoRoute.GET("/:id")
+		todoRoute.PUT("/:id")
+		todoRoute.DELETE("/:id")
+	}
+}
+
+func (h *TodoHandler) CreateTask(ctx *gin.Context) { panic("implement me") }
+func (h *TodoHandler) GetTask(ctx *gin.Context)    { panic("implement me") }
+func (h *TodoHandler) UpdateTask(ctx *gin.Context) { panic("implement me") }
+func (h *TodoHandler) DeleteTask(ctx *gin.Context) { panic("implement me") }
